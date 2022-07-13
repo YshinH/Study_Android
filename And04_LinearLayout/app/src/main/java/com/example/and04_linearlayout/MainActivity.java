88f02b4cfa43de02ddf0305bf0dcae115cad0f58
YshinH <tlsgid666@gmail.com>
2022-07-13 17:50:25 +0900

    2022-07-13

--- And04_LinearLayout/app/src/main/java/com/example/and04_linearlayout/MainActivity.java
+++ And04_LinearLayout/app/src/main/java/com/example/and04_linearlayout/MainActivity.java
@@ -1,18 +1,14 @@
-package com.example.and01_helloworld;
+package com.example.and04_linearlayout;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);/* 디자인 파일과 연결 setContentView*/
-
+        setContentView(R.layout.activity_main);
     }
 }
